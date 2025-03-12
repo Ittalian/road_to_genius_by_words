8ef7d60b62cbcec56442a9d714fe3a4bfe06b4f7
Ittalian <bothmather@au.com>
2025-03-13 02:57:50 +0900

    インデント修正

--- lib/views/widgets/memorize_selection.dart
+++ lib/views/widgets/memorize_selection.dart
@@ -48,74 +48,74 @@ class MemorizeSelectionState extends State<MemorizeSelection> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          spacing: 10,
-          runSpacing: 10,
-          children: [
-            Container(
-              color: Colors.white.withOpacity(0.5),
-              padding: const EdgeInsets.fromLTRB(0, 10, 6, 10),
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (!widget.answeredAll)
-                    Text(
-                      widget.word.value,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        spacing: 10,
+        runSpacing: 10,
+        children: [
+          Container(
+            color: Colors.white.withOpacity(0.5),
+            padding: const EdgeInsets.fromLTRB(0, 10, 6, 10),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (!widget.answeredAll)
+                  Text(
+                    widget.word.value,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                  const Padding(padding: EdgeInsets.only(top: 20)),
-                  if (!widget.answeredAll)
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        for (var suggestion in widget.suggestions) ...{
-                          Radio<String>(
-                            value: suggestion,
-                            groupValue: answer,
-                            onChanged: (String? value) {
-                              setState(() {
-                                if (value != null) {
-                                  answer = value;
-                                }
-                              });
-                            },
-                          ),
-                          Expanded(
-                            child: Text(
-                              suggestion,
-                              softWrap: true,
-                              overflow: TextOverflow.visible,
-                              style: const TextStyle(
-                                fontSize: 13,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        },
-                      ],
-                    ),
-                  const Padding(padding: EdgeInsets.only(top: 10)),
-                  BaseButton(
-                    label: widget.answeredAll ? '解答を見る' : '回答',
-                    onPressed: widget.answeredAll
-                        ? () => widget.finishMemorize()
-                        : () {
-                            widget.onPressedAnswer(answer);
+                  ),
+                const Padding(padding: EdgeInsets.only(top: 20)),
+                if (!widget.answeredAll)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (var suggestion in widget.suggestions) ...{
+                        Radio<String>(
+                          value: suggestion,
+                          groupValue: answer,
+                          onChanged: (String? value) {
                             setState(() {
-                              answer = '';
+                              if (value != null) {
+                                answer = value;
+                              }
                             });
                           },
+                        ),
+                        Expanded(
+                          child: Text(
+                            suggestion,
+                            softWrap: true,
+                            overflow: TextOverflow.visible,
+                            style: const TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      },
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                BaseButton(
+                  label: widget.answeredAll ? '解答を見る' : '回答',
+                  onPressed: widget.answeredAll
+                      ? () => widget.finishMemorize()
+                      : () {
+                          widget.onPressedAnswer(answer);
+                          setState(() {
+                            answer = '';
+                          });
+                        },
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
